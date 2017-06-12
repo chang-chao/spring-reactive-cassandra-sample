28597b4a4fc196c5a63624e2eea0d9f58fdf49a6
chang chao <chang-chao@hotmail.com>
2017-06-12 13:49:58 +0900

    update to latest

--- src/main/java/chao/playground/spring/reactive/jdbc/web/StudentController.java
+++ src/main/java/chao/playground/spring/reactive/jdbc/web/StudentController.java
@@ -8,24 +8,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import chao.playground.spring.reactive.jdbc.model.Student;
 import chao.playground.spring.reactive.jdbc.model.dao.StudentDAO;
+import lombok.extern.apachecommons.CommonsLog;
 import rx.Observable;
 
 @RestController
 @RequestMapping(value = "/jdbc/student")
+@CommonsLog
 public class StudentController {
   @Autowired
   private StudentDAO studentDao;
 
   @RequestMapping(value = "/")
   public Observable<Student> list() {
-    return studentDao.getAllStudents();
+    log.debug("list start");
+    Observable<Student> list = studentDao.getAllStudents();
+    log.debug("list end");
+    return list;
   }
 
   @RequestMapping(value = "/save")
   public Observable<Integer> save() {
+    log.debug("save start");
     Student student = new Student();
     student.setName("randomName:" + new Random().nextDouble());
-    return studentDao.save(student);
+    Observable<Integer> studentId = studentDao.save(student);
+    log.debug("save end");
+    return studentId;
   }
 
 }
