3ca690e232dcd87a30d291ab626ffb13f6c44820
chao.chang <chao.chang@linecorp.com>
2018-10-09 11:39:35 +0900

    Polish.

--- src/main/java/chao/playground/spring/reactive/cassandra/web/person/PersonController.java
+++ src/main/java/chao/playground/spring/reactive/cassandra/web/person/PersonController.java
@@ -1,7 +1,6 @@
 package chao.playground.spring.reactive.cassandra.web.person;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,26 +13,24 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping(value = "/person")
-@EnableAutoConfiguration
 public class PersonController {
 
-  @Autowired
-  private PersonRepository personRepository;
+	@Autowired
+	private PersonRepository personRepository;
 
-  @RequestMapping(value = "/")
-  public Flux<Person> list() {
-    return personRepository.findAll();
-  }
+	@RequestMapping(value = "/")
+	public Flux<Person> list() {
+		return personRepository.findAll();
+	}
 
-  @RequestMapping(value = "/add")
-  public Mono<Person> add() {
-    Person alice = new Person(UUIDs.timeBased(), "Alice");
-    return personRepository.insert(alice);
-  }
+	@RequestMapping(value = "/add")
+	public Mono<Person> add() {
+		Person alice = new Person(UUIDs.timeBased(), "Alice");
+		return personRepository.insert(alice);
+	}
 
-  @GetMapping(path = "/sse", produces = "text/event-stream")
-  public Flux<Person> getPersons() {
-    return this.personRepository.findAll();
-  }
+	@GetMapping(path = "/sse", produces = "text/event-stream")
+	public Flux<Person> getPersons() {
+		return this.personRepository.findAll();
+	}
 }
